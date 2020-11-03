bcc29e2ba1e89c9213f79ca74ec9141093577347
Unknown <unknown@example.com>
2020-11-03 17:40:00 +0200

    improved_charging

--- drivers/power/supply/qcom/step-chg-jeita.c
+++ drivers/power/supply/qcom/step-chg-jeita.c
@@ -124,8 +124,57 @@ static struct jeita_fcc_cfg jeita_fcc_config = {
 		/* TEMP_LOW	TEMP_HIGH	FCC */
 		{0,		50,		400000},
 		{51,		150,		1200000},
-		{151,		450,		2500000},
-		{451,		600,		1200000},
+		{151,		430,		2400000},
+		{431,		450,		2000000},
+		{451,		470,		1500000},
+		{471,		600,		 800000},
+	},
+};
+#elif defined(CONFIG_KERNEL_CUSTOM_F7A)
+static struct jeita_fcc_cfg jeita_fcc_config = {
+	.psy_prop	= POWER_SUPPLY_PROP_TEMP,
+	.prop_name	= "BATT_TEMP",
+	.hysteresis	= 0, /* 1degC hysteresis */
+	.fcc_cfg	= {
+		/* TEMP_LOW	TEMP_HIGH	FCC */
+		{0,			50,			 400000},
+		{51,		150,		1200000},
+		{151,		430,		2400000},
+		{431,		450,		2000000},
+		{451,		470,		1500000},
+		{471,		600,		 800000},
+	},
+};
+#elif defined(CONFIG_KERNEL_CUSTOM_E7S)
+static struct jeita_fcc_cfg jeita_fcc_config = {
+	.psy_prop	= POWER_SUPPLY_PROP_TEMP,
+	.prop_name	= "BATT_TEMP",
+	.hysteresis	= 0, /* 1degC hysteresis */
+	.fcc_cfg	= {
+		/* TEMP_LOW	TEMP_HIGH	FCC */
+		{0,			50,			 400000},
+		{51,		150,		1200000},
+		{151,		400,		2400000},
+		{401,		430,		2000000},
+		{431,		450,		1500000},
+		{451,		470,		1000000},
+		{471,		600,		 600000},
+	},
+};
+#elif defined(CONFIG_KERNEL_CUSTOM_E7T)
+static struct jeita_fcc_cfg jeita_fcc_config = {
+	.psy_prop	= POWER_SUPPLY_PROP_TEMP,
+	.prop_name	= "BATT_TEMP",
+	.hysteresis	= 0, /* 1degC hysteresis */
+	.fcc_cfg	= {
+		/* TEMP_LOW	TEMP_HIGH	FCC */
+		{0,			50,			 400000},
+		{51,		150,		1200000},
+		{151,		400,		2400000},
+		{401,		430,		2000000},
+		{431,		450,		1500000},
+		{451,		470,		1000000},
+		{471,		600,		 600000},
 	},
 };
 #endif
